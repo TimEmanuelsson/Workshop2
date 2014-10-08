55a5fbadffc413cacd1c6a906ab03298e05bfa70
Emil Dannberger <ed222an@student.lnu.se>
2014-10-08 11:17:02 +0200

    Rendering av medlem färdig.

--- src/member/view/MemberView.php
+++ src/member/view/MemberView.php
@@ -18,8 +18,36 @@ Class MemberView {
 	
 	public function showMember($member)
 	{
-		var_dump($member);
+		$contentString = "
+			<h4>Member Information</h4>
+			<ul>
+				<li>MemberID: " . $member->getID() . "</li>
+				<li>Personal Identity Number: " . $member->getIdentityNumber() . "</li>
+			</ul>
+			<h4>" . utf8_encode($member->getFirstName()) . "'s boats</h4>
+			<ul>
+				";
 		
-		// TODO: Plocka ut data ur $member och lägg in i html som skall returneras till kontrollern.
+		if(count($member->getBoats() <= 0) && $member->getBoats != NULL)
+		{
+			$contentString .= "<li>" . utf8_encode($member->getFirstName()) . " does not have any registered boats</li>";
+		}
+		else
+		{
+			foreach ($member->getBoats() as $boat)
+			{
+				$contentString .= "<li>Boat type: " . $boat-getBoatType() . " Boat length: " . $boat->getLength() . "</li>";
+			}
+		}
+		
+		$contentString .= "</ul>";
+		
+		$ret = "
+				<h1>Specific Member - " . utf8_encode($member->getFirstName()) . " " . utf8_encode($member->getLastName()) . "</h1>
+				<a href='?CompactList'>Show Compact List</a>
+				<a href='?DetailedList'>Show Detailed List</a>
+				" . $contentString;
+		
+		return $ret;
 	}
 }
